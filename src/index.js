9d3d166f973bd5c69621780f39d0ba6851821795
“Erno <ernohilarion@MacBook-Air-de-Erno.local>
2024-04-18 20:40:42 +0200

    timer

--- src/index.js
+++ src/index.js
@@ -6,33 +6,67 @@ let timer = null; // Variable to store the interval
 
 // ITERATION 1: Add event listener to the start button
 
-// Your code goes here ...
-
+// startCountdown()
+//   document.getElementById("start-btn").addEventListener("click", startCountdown);
+// }
 
 
 
 // ITERATION 2: Start Countdown
+document.querySelector("#start-btn").addEventListener("click", startCountdown);
+
+
 function startCountdown() {
   console.log("startCountdown called!");
 
 
-  // Your code goes here ...
+  document.getElementById("start-btn").disabled = true;
+
+  const countDown = setInterval(() => {
+    remainingTime--
+    document.querySelector('#time').innerText = remainingTime
+  }, 1000);
+
+
+
+  setTimeout(() => {
+    clearInterval(countDown);
+    showToast()
+  }, 10000);
 }
+
+
+
+
+
+// let counterValue = 0
+
+// const intervalID = setInterval(() => {
+//   counterValue++
+//   document.querySelector("#time")).innerText = counterValue
+// }, 1000)
+
+
+
 
 
 
 
 // ITERATION 3: Show Toast
+
+
+const toastMsg = document.querySelector("#toast");
 function showToast(message) {
-  console.log("showToast called!");
-
-  // Your code goes here ...
-
-
-
-
-  // BONUS: ITERATION 4: TOAST CLOSE BUTTON
-
-  // Your code goes here ...
-
+  console.log("showtoast called");
+  toastMsg.setAttribute('class', 'toast show')
 }
+
+
+
+
+
+// BONUS: ITERATION 4: TOAST CLOSE BUTTON
+
+// Your code goes here ...
+
+
